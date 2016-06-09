7dfe5a1801d22bb67e1f2cff820a70a52cca5451
EriksonDinossauro <eriksonpedro65ma@gmail.com>
2016-06-09 10:27:21 -0300

    codigo ola mundo
    codigo comentado

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+//Meu Primeiro Codigo em C++
+#include <iostream>
+
+using namespace std;
+//declarando a variavel
+int main()
+ {
+	//imprimindo na tela
+		cout << "Hello World" << endl;
+	
+		
+		return 0;
+	
+		
+	
+		}
